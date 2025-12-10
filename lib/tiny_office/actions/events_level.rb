274acd12776fc9dfbb3f437483411ff9643f3b2d
MaTsou <contact@logicore.fr>
2025-12-10 12:11:55 +0100

    Add a default html_wrapper and remove method_missing in events_level (was stupid!)

--- lib/tiny_office/actions/events_level.rb
+++ lib/tiny_office/actions/events_level.rb
@@ -1,9 +1,9 @@
 module TinyOffice
+  # I use a class rather than simply 'symbols' or 'string' to raise an error on 
+  # wrong params..
   class EventsLevel
     def self.full; 'full'; end
     def self.read_only; 'read-only'; end
     def self.no_event; 'no-event'; end
-
-    def self.method_missing(name); 'no-event'; end
   end
 end
--- lib/tiny_office/configuration.rb
+++ lib/tiny_office/configuration.rb
@@ -4,5 +4,9 @@ module TinyOffice
       :html_wrapper, :token_builder, :action_defaults, :logo,
       :onlyoffice_url, :onlyoffice_js_cdn
     )
+
+    def html_wrapper
+      @html_wrapper ||= Wrapper::FullPage.new
+    end
   end
 end
